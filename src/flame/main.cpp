d70191957a5512efdfd3f31dd0ca8171d3e3dc6f
アクゼスティア <akzestia@gmail.com>
2025-08-18 04:04:31 +0900

    main.cpp for the flame shader

---
+++ src/flame/main.cpp
@@ -0,0 +1,19 @@
+
+#include <QtQml/qqmlregistration.h>
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char* argv[]) {
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    engine.loadFromModule("flame", "Main");
+
+    return app.exec();
+}
